98b2e7872f4ee2d1bed93f1dfc9c56668868ea43
Kalai1415 <kalaiaug14@gmail.com>
2022-07-19 13:51:06 +0530

    User Detail

---
+++ src/org/test/Sample1.java
@@ -0,0 +1,21 @@
+package org.test;
+
+public class Sample1 {
+	
+	private void Username() {
+System.out.println("Username is Kalai");
+	}
+
+	private void Password() {
+System.out.println("Password is 987");
+	}
+	
+	public static void main(String[] args) {
+		
+		Sample1 s =new Sample1();
+		s.Username();
+		s.Password();
+		
+		
+	}
+}
